40fb5c2d358bf33c13415273456af141c678200b
Dennish02 <dennishesler02@gmail.com>
2022-05-07 18:30:37 -0300

    ggg

--- frontend/src/componentes/landing/Formulario.jsx
+++ frontend/src/componentes/landing/Formulario.jsx
@@ -11,7 +11,7 @@ export default function Formulario() {
                  </p>
                 </div>
                 <button className='buttonPrimary'>
-                    LOGUIN TO GOOGLE
+                    Explore
                 </button>
             </div>
         </div>
