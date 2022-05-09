8a7ce3945ee5580d75e142cdc2f5fd350db74a63
Dennis Nicolas Hesler <44222925+Dennish02@users.noreply.github.com>
2022-05-09 13:25:16 -0300

    Delete backend directory
    delete main

--- backend/helpers/generarId.js
+++
@@ -1,9 +0,0 @@
-const generarID = ()=>{
-   const random = Math.random().toString(32).substring(2)//cadena random de letras- dos primeros lugarres
-   const date = Date.now().toString(32);
-   return random + date;
-}
-
-export {
-    generarID
-}
--- backend/helpers/generarJWT.js
+++
@@ -1,8 +0,0 @@
-import  jwt  from "jsonwebtoken";
-
- const generarJWT = (id)=>{
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: '1d' //tiempo de valides
-    })
- }
- export default generarJWT;
