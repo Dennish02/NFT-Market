5d8df52dc5108f77c30fc83a4c7847c1e0f73dc7
Pablo-Zalazar <pablo_zalazar@outlook.com>
2022-05-10 12:24:07 -0300

    commit

--- frontend/redux/actions/actionUSER.js
+++ frontend/redux/actions/actionUSER.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import profile1 from "../../src/img/profile1.png";
+import profile2 from "../../src/img/profile2.png";
+import profile3 from "../../src/img/profile3.png";
 
 // export function allNftMarket() {
 //   return async function (dispatch) {
@@ -15,18 +18,23 @@ import axios from "axios";
 // }
 
 export function registroUsuario({ nombre, email, password }) {
-  console.log(nombre);
-  console.log(email);
-  console.log(password);
+  const n = Math.floor(Math.random() * 10) % 3;
+
   return async function () {
     try {
       const body = {
         nombre,
         email,
         password,
-        image: "ruta",
+        image:
+          n === 0
+            ? profile1.toString()
+            : n === 1
+            ? profile2.toString()
+            : profile3.toString(),
         coins: 1000,
       };
+      console.log(body);
       const response = await axios.post(
         "http://localhost:3001/api/usuario",
         body
