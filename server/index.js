4ed0de3cdac3ebc86b02159f300ea3f69234e05b
adelgadilloc <delgadilloabel20@gmail.com>
2022-05-10 12:54:30 -0300

    m

--- server/index.js
+++ server/index.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000; // Puerto que vamos a usar
-const USER = process.env.USER; // SI ACABAN DE CLONAR EL REPO TIENEN QUE CREAR SU .env CON LAS VARIABLES USER Y PASSWORD, AHÍ TIENEN QUE PONER LAS QUE TIENEN EN MONGO ATLAS
-const PASSWORD = process.env.PASSWORD;
-const CONNECTION_URL = `mongodb+srv://${USER}:${PASSWORD}@cluster0.fnmuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` // Conectamos con nuestro usuario de Atlas
+const DB_PORT = process.env.DB_PORT || 5000; // Puerto que vamos a usar
+const DB_USER = process.env.DB_USER; // SI ACABAN DE CLONAR EL REPO TIENEN QUE CREAR SU .env CON LAS VARIABLES USER Y PASSWORD, AHÍ TIENEN QUE PONER LAS QUE TIENEN EN MONGO ATLAS
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const CONNECTION_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.fnmuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` // Conectamos con nuestro usuario de Atlas
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}) // Conectamos con la DB y nos prevenimos de algunos errores
-.then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))) // Nos devuelve una promesa así que hacemos un .then
+.then(() => app.listen(DB_PORT, () => console.log(`Server running on Port: ${DB_PORT}`))) // Nos devuelve una promesa así que hacemos un .then
 .catch((error) => console.log(error.message)) // Manejo de error
