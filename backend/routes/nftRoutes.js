c05d8d0289f1ef85e3d42bcf88d83f1a2e3c10b7
Pablo-Zalazar <pablo_zalazar@outlook.com>
2022-05-10 19:24:16 -0300

    commit

--- backend/routes/nftRoutes.js
+++ backend/routes/nftRoutes.js
@@ -1,29 +1,29 @@
-import express from 'express';
-const router = express.Router()
-import checkOut from '../middleware/checkOut.js'
+import express from "express";
+const router = express.Router();
+import checkOut from "../middleware/checkOut.js";
 
-import {  obtenerAllNft,
-    crearNft,
-    editarNft,
-    regalarNft,
-    comprarNft,
-    venderNft,
-    añadirFavNft,
-    allNftUser,
-    obtenerNft} from '../controladores/nftController.js'
+import {
+  obtenerAllNft,
+  crearNft,
+  editarNft,
+  regalarNft,
+  comprarNft,
+  venderNft,
+  añadirFavNft,
+  allNftUser,
+  obtenerNft,
+} from "../controladores/nftController.js";
 
-router.route('/')
-    .get(obtenerAllNft)
-    .post(checkOut, crearNft)
-router.get('/portfolio',checkOut, allNftUser)   
+router.route("/").get(obtenerAllNft).post(checkOut, crearNft);
+router.get("/portfolio", checkOut, allNftUser);
 
-router.route('/:id')
-    .get(checkOut, obtenerNft)
-    .get(checkOut, regalarNft)
-    .put(checkOut, editarNft)
-    .post(checkOut, comprarNft)
-    .post(checkOut, venderNft)
-    .post(checkOut,añadirFavNft)
+router
+  .route("/:id")
+  .get(checkOut, obtenerNft)
+  .get(checkOut, regalarNft)
+  .put(checkOut, editarNft)
+  .post(checkOut, comprarNft)
+  .post(checkOut, venderNft)
+  .post(checkOut, añadirFavNft);
 
-
- export default router;   
+export default router;
