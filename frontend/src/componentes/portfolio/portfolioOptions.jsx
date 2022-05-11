7b9da22350aefdbd5d69be7bd82f31b970663df6
Dennish02 <dennishesler02@gmail.com>
2022-05-11 17:37:02 -0300

    estilos en portfolio

--- frontend/src/componentes/portfolio/portfolioOptions.jsx
+++ frontend/src/componentes/portfolio/portfolioOptions.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 
 export default function PortfoliOptions() {
   return (
-    <div >
-    <button className='portfolioCreateNft'>CREAR NFT</button>
+    <div  >
+      <div className='contButton'>
+      <button className='portfolioCreateNft'>CREAR NFT</button>
     <button className='portfolioCreateColection'>CREAR COLECCION</button>
     <button className='portfolioFavoritos'>MIS FAVORITOS</button>
-    <div>
+      </div>
+    
+    <div className='contTittle'>
         
         <h2 className='tuPortfolio'>your portfolio</h2> 
+
+        <div>
         <select className='coleccion' name="colection" id="">
         <option value="colection">coleccion</option>
         </select>
+        </div>
+        
     </div>
 
     </div>
