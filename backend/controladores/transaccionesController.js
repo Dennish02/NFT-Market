3c9b0def10d455288e43eb16c70ee0b757567c6c
adelgadilloc <delgadilloabel20@gmail.com>
2022-05-13 12:20:06 -0300

    manejo de errores

--- backend/controladores/transaccionesController.js
+++ backend/controladores/transaccionesController.js
@@ -1,21 +1,28 @@
 import Transaccion from "../models/transaccion.js";
 
 const ultimasVentas = async (req,res) => {
-    const allTransactions = await Transaccion.find()
-    const sales = allTransactions.filter(t => t.transactionType === 'sale')
-    const orderedSales = sales.sort((a,b) => {
-        //los ordeno del más reciente al más antiguo
-        return b.createdAt.getTime() - a.createdAt.getTime()
-    })
-    const recentTransactions = orderedSales.slice(0, 20)
-    res.json(recentTransactions)
-
+    try{
+        const allTransactions = await Transaccion.find()
+        const sales = allTransactions.filter(t => t.transactionType === 'sale')
+        const orderedSales = sales.sort((a,b) => {
+            //los ordeno del más reciente al más antiguo
+            return b.createdAt.getTime() - a.createdAt.getTime()
+        })
+        const recentTransactions = orderedSales.slice(0, 20)
+        res.status(200).send(recentTransactions)
+    } catch (error){
+        res.status(400).send(error)
+    }
 }
 
 const crearTransaccion = async (req, res) => {
-    const transaccion = new Transaccion(req.body)
-    const transaccionGuardada = await transaccion.save()
-    res.json(transaccionGuardada)
+    try{
+        const transaccion = new Transaccion(req.body)
+        const transaccionGuardada = await transaccion.save()
+        res.status(200).send(transaccionGuardada)
+    } catch (error) {
+        res.status(400).send(error)
+    }
 }
 
 export {
