ba599f8a7d0185698e52ae8d06e3e2535ae6ebbb
Dennis Nicolas Hesler <dennishesler02@gmail.com>
2022-05-15 14:13:33 -0300

    estilos-porfolio

--- frontend/src/componentes/portfolio/UserNFT.jsx
+++ frontend/src/componentes/portfolio/UserNFT.jsx
@@ -1,22 +1,19 @@
 import React from "react";
+import formateoPrecio from "../../middleware/formateoPrecio";
 
 export default function ComponentNFT(props) {
-  const { id, image, colection, category, price, creatorId, ownerId } = props;
-
+  const { id, image, colection, price,priceBase, creatorId, ownerId } = props;
+ 
   return (
     <div className="contNFT">
-      <img src={image.url} alt="NFT IMAGE" />
+      <div className="contImg">
+      <img src={image.url} alt="NFT IMAGE" height='280px' />
+      </div>
       <div className="contNFTinfo">
         <h2>{`${colection}  ${id}`}</h2>
         <p>{`creator:  ${creatorId}`}</p>
-        <p>
-          {" "}
-          owner: <small> {ownerId}</small>
-        </p>
-        <p>
-          {" "}
-          price: <span> {`${price} CL`}</span>{" "}
-        </p>
+        <p>owner: <small> {ownerId}</small></p>
+        <div className="contPrice"><p>price:</p>  <span> {formateoPrecio(price)}</span></div>
       </div>
       <div className="contButtons">
         <button className="w-50 buttonPrimary">EDITAR</button>
--- frontend/src/middleware/formateoPrecio.jsx
+++ frontend/src/middleware/formateoPrecio.jsx
@@ -3,8 +3,8 @@ import icon from '../img/cl.png'
 export default function formateoPrecio(number) {
    
         return(
-            <div className='contIcon'>
+            <p className='contIcon'>
             {number} <img className='iconcl' src={icon} width="20px"/> 
-            </div>
+            </p>
         )
 }
