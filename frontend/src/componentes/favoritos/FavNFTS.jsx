609f621287eb16473dfcb9b5642f530bb739e9fc
Gabriel <pein4302@gmail.com>
2022-05-18 20:35:49 -0300

    componente favoritos

--- frontend/src/componentes/favoritos/FavNFTS.jsx
+++ frontend/src/componentes/favoritos/FavNFTS.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function    FavnFTS() {
+function    FavnFTS({id, image, _id, colection, avaliable, priceBase, price, creatorId,ownerId}) {
   return (
-    <div>TUVIEJA</div>
+    <div className="contNFT">
+      <img className='contImg' src={image.url} alt="not image"  />
+      <div className="contNFTinfo">
+      <h2>{`${colection} ${id}`}</h2>
+      <p>{`creator: ${creatorId}`}</p>
+      <p>
+          {" "}
+          owner: <small> {ownerId}</small>{" "}
+        </p>
+
+        <p>{avaliable ? "En venta" : "No en venta"}</p>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Favoritos.jsx
+++ frontend/src/pages/Favoritos.jsx
@@ -2,16 +2,37 @@ import React from 'react'
 import NavBar from '../componentes/home/NavBar'
 import { Link } from 'react-router-dom'
 import FavNFTS from '../componentes/favoritos/FavNFTS'
-export default function Favoritos() {
-  return (
-    <div>IM FAVORITOS
-        <NavBar/>
-        <FavNFTS/>
-        <h1>Favorites </h1>
+import { useSelector } from 'react-redux'
 
-        {/* ACA SE TIENEN QUE RENDERIZAR LOS NFT QUE ESTEN AGREGADOS A FAVORITOS  */}
-        <Link to ='/home/usuario/portfolio'> <button>VOLVER AL PORTFOLIO </button></Link>
-        
-    </div>
-  )
+export default function Favoritos() {
+    const miUser = useSelector(state => state.usuario)
+    const favoritos = miUser.favoritos
+    const prueba = favoritos.map(el => el.image.url)
+    console.log('soy favoritos', prueba)
+    return (
+        <div><h1>favoritos</h1>
+            <NavBar />
+            {favoritos.length > 0 ? favoritos.map(fav => {
+                return (
+                    <FavNFTS image={fav.image} id={fav.id} 
+                    colection={fav.colection} avaliable={fav.avaliable}
+                    creatorId={fav.creatorId} ownerId={fav.ownerId}
+                    />
+                )
+            })
+
+
+
+
+                : <p>no hay favoritos </p>
+
+            }
+
+            <h1>Favorites </h1>
+
+            {/* ACA SE TIENEN QUE RENDERIZAR LOS NFT QUE ESTEN AGREGADOS A FAVORITOS  */}
+            <Link to='/home/usuario/portfolio'> <button>VOLVER AL PORTFOLIO </button></Link>
+
+        </div>
+    )
 }
