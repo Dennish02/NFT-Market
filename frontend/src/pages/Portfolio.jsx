c08879f748c91e335d6851b377df7b040a16192d
Pablo-Zalazar <pablo_zalazar@outlook.com>
2022-05-19 18:56:53 -0300

    arreglos portfolio

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -27,9 +27,9 @@ export default function Portfolio() {
     socket.on("nftUser", () => {
       dispatch(allNFTUser());
     });
-    // socket.on("colectionUser", () => {
-    //   dispatch(coleccionesUsuario());
-    // });
+    socket.on("colectionUser", () => {
+      dispatch(coleccionesUsuario());
+    });
   });
 
   return (
