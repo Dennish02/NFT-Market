88780ed84ef9d682597f71da25259927bfd978b2
Dennis Nicolas Hesler <dennishesler02@gmail.com>
2022-05-19 14:37:12 -0300

    resolving the plainign of the King the my herat

--- backend/routes/mercadoPago.js
+++ backend/routes/mercadoPago.js
@@ -2,10 +2,10 @@ import express from "express";
 const router = express.Router();
 import checkOut from "../middleware/checkOut.js";
 
-import { mercadoPago } from "../controladores/mercadopago.js";
+import { payMercadoPago, setCoins } from "../controladores/payMercadoPago.js";
 
-router.post("/", checkOut, mercadoPago);
-
+router.post("/", payMercadoPago);
+router.put("/setcoins",checkOut, setCoins);
 // router.post(checkOut, añadirFavNft);
 
 export default router;
